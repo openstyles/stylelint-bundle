20839f1584f65505e4b293bcdab67a284d902e16
eight04 <eight04@gmail.com>
2022-11-11 12:40:49 +0800

    Add: sugarss

--- demo/demo.js
+++ demo/demo.js
@@ -3,51 +3,53 @@ const DEFAULT_SOURCE = `body {
   color: #FFDFF;
 }`;
 
-const CONFIG = {rules: {
-  // https://github.com/stylelint/stylelint-config-recommended/blob/main/index.js
-  'annotation-no-unknown': true,
-  'at-rule-no-unknown': true,
-  'block-no-empty': true,
-  'color-no-invalid-hex': true,
-  'comment-no-empty': true,
-  'custom-property-no-missing-var-function': true,
-  'declaration-block-no-duplicate-custom-properties': true,
-  'declaration-block-no-duplicate-properties': [
-    true,
-    {
-      ignore: ['consecutive-duplicates-with-different-values'],
-    },
-  ],
-  'declaration-block-no-shorthand-property-overrides': true,
-  'font-family-no-duplicate-names': true,
-  'font-family-no-missing-generic-family-keyword': true,
-  'function-calc-no-unspaced-operator': true,
-  'function-linear-gradient-no-nonstandard-direction': true,
-  'function-no-unknown': true,
-  'keyframe-block-no-duplicate-selectors': true,
-  'keyframe-declaration-no-important': true,
-  'media-feature-name-no-unknown': true,
-  'named-grid-areas-no-invalid': true,
-  'no-descending-specificity': true,
-  'no-duplicate-at-import-rules': true,
-  'no-duplicate-selectors': true,
-  'no-empty-source': true,
-  'no-extra-semicolons': true,
-  'no-invalid-double-slash-comments': true,
-  'no-invalid-position-at-import-rule': true,
-  'no-irregular-whitespace': true,
-  'property-no-unknown': true,
-  'selector-pseudo-class-no-unknown': true,
-  'selector-pseudo-element-no-unknown': true,
-  'selector-type-no-unknown': [
-    true,
-    {
-      ignore: ['custom-elements'],
-    },
-  ],
-  'string-no-newline': true,
-  'unit-no-unknown': true,
-}};
+const CONFIG = {
+  rules: {
+    // https://github.com/stylelint/stylelint-config-recommended/blob/main/index.js
+    'annotation-no-unknown': true,
+    'at-rule-no-unknown': true,
+    'block-no-empty': true,
+    'color-no-invalid-hex': true,
+    'comment-no-empty': true,
+    'custom-property-no-missing-var-function': true,
+    'declaration-block-no-duplicate-custom-properties': true,
+    'declaration-block-no-duplicate-properties': [
+      true,
+      {
+        ignore: ['consecutive-duplicates-with-different-values'],
+      },
+    ],
+    'declaration-block-no-shorthand-property-overrides': true,
+    'font-family-no-duplicate-names': true,
+    'font-family-no-missing-generic-family-keyword': true,
+    'function-calc-no-unspaced-operator': true,
+    'function-linear-gradient-no-nonstandard-direction': true,
+    'function-no-unknown': true,
+    'keyframe-block-no-duplicate-selectors': true,
+    'keyframe-declaration-no-important': true,
+    'media-feature-name-no-unknown': true,
+    'named-grid-areas-no-invalid': true,
+    'no-descending-specificity': true,
+    'no-duplicate-at-import-rules': true,
+    'no-duplicate-selectors': true,
+    'no-empty-source': true,
+    'no-extra-semicolons': true,
+    'no-invalid-double-slash-comments': true,
+    'no-invalid-position-at-import-rule': true,
+    'no-irregular-whitespace': true,
+    'property-no-unknown': true,
+    'selector-pseudo-class-no-unknown': true,
+    'selector-pseudo-element-no-unknown': true,
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignore: ['custom-elements'],
+      },
+    ],
+    'string-no-newline': true,
+    'unit-no-unknown': true,
+  }
+};
 
 const textarea = document.querySelector("textarea");
 const output = document.querySelector("output");
@@ -67,10 +69,14 @@ textarea.addEventListener("change", () => {
 textarea.value = DEFAULT_SOURCE;
 update();
 function update() {
+  const code = textarea.value;
   timer = null;
   stylelint.lint({
-    config: CONFIG,
-    code: textarea.value,
+    config: {
+      ...CONFIG,
+      customSyntax: code.includes("{") ? null : "sugarss"
+    },
+    code,
     formatter: () => {}
   })
     .then(result => {
--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-export {default} from "stylelint";
-
+import stylelint from "stylelint";
+import sugarss from "sugarss";
+const _lint = stylelint.lint;
+stylelint.lint = options => {
+  if (options?.config?.customSyntax === "sugarss") {
+    options.config.customSyntax = sugarss;
+  }
+  return _lint.call(stylelint, options);
+}
+export {stylelint as default};
